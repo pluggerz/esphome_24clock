926cf9292db0cd2b9dc511e5f409e00c49b4ad3f
Henri <h.vd.esker@gmail.com>
2022-12-13 14:30:07 +0100

    support for time

--- components/24clock/__init__.py
+++ components/24clock/__init__.py
@@ -1,5 +1,7 @@
 import esphome.codegen as cg
 from esphome.voluptuous_schema import _Schema
+from esphome.components import time as time_
+import esphome.config_validation as cv
 
 DEPENDENCIES = ["pcf8574"]
 
@@ -9,8 +11,11 @@ CODEOWNERS = ["@hvandenesker"]
 
 oclock_ns = cg.esphome_ns.namespace("24clock")
 
+CONF_TIME_ID = 'time_id'
+
 CONFIG_SCHEMA = _Schema(
     {
+        cv.Optional(CONF_TIME_ID, default='current_time'): cv.use_id(time_.RealTimeClock),
 #        cv.GenerateID(): cv.declare_id(OClockStubController),
 #        cv.Optional(CONF_TIME_ID): cv.use_id(time.RealTimeClock),
 #        cv.Optional(CONF_COUNT_START, -1): cv.int_range(min=-1, max=64),
